7d89aee644861dfd19db1b07ab4bd8748de314e7
Jakub Smolik <smolikja@protonmail.com>
2025-12-19 06:18:54 +0100

    commenting out "cs" locale - multilingual summary is not ready yet

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
     // A list of all locales that are supported
-    locales: ["en", "cs"],
+    locales: ["en"], // TODO: add "cs" when ready
     // Used when no locale matches
     defaultLocale: "en",
     // Automatically detect user's preferred locale
